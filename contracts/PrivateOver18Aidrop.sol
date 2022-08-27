3050e96e67765040c288c1334b53b32ca7967404
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2022-08-27 14:46:29 +0200

    Update PrivateOver18Aidrop.sol
    Update Smart Contract logic to accomodate the new verifier.sol

--- contracts/PrivateOver18Aidrop.sol
+++ contracts/PrivateOver18Aidrop.sol
@@ -9,7 +9,7 @@ interface IVerifier {
     function verifyProof(uint[2] memory a,
             uint[2][2] memory b,
             uint[2] memory c,
-            uint[72] memory input)
+            uint[73] memory input)
         external view returns(bool);
 }
 
@@ -38,7 +38,7 @@ contract PrivateOver18Airdrop {
     function collectAirdrop(uint[2] memory a,
             uint[2][2] memory b,
             uint[2] memory c,
-            uint[72] memory input,
+            uint[73] memory publicValues,
             uint256 _tokenID
             ) public 
     {   
@@ -47,15 +47,16 @@ contract PrivateOver18Airdrop {
         // @dev check if the public input used to generate the proof are legit
         require(isElegibleForAirdrop[msg.sender] == false, "You already claimed your airdrop!");
         require(privateSoulMinter.ownerOf(_tokenID) == msg.sender,"This token belongs to another soul");
-        require(privateSoulMinter.claimSignatureHash(_tokenID) == keccak256(abi.encodePacked(input[0], input[1], input[2])), "The signature is not valid");
-        require(input[3] == 0x152f5044240ef872cf7e6742fe202b9e07ed6188e9e734c09b06939704852358 && input[4] == 0x2865441cd3e276643c84e55004ad259dff282c8c47c6e8c151afacdadf6f6db3, "The claim was signed by a wrong public key");
-        require(input[5] == 0x0000000000000000000000000000000087b9b4c689c2024c54d1bf962cb16bce, "Wrong Claim schema used to generate the proof");
-        require(input[6] == 0x0000000000000000000000000000000000000000000000000000000000000002, "Invalid slot index used to generate the proof");
-        require(input[7] == 0x0000000000000000000000000000000000000000000000000000000000000003, "Invalid operator used to generate the proof");
-        require(input[8] == 0x0000000000000000000000000000000000000000000000000000000000000012, "Invalid threshold value used to generate the proof");
+        require(publicValues[0] == 0x0000000000000000000000000000000000000000000000000000000000000001, "The claim doesn't satisfy the query condition");
+        require(privateSoulMinter.claimSignatureHash(_tokenID) == keccak256(abi.encodePacked(publicValues[1], publicValues[2], publicValues[3])), "The signature is not valid");
+        require(publicValues[4] == 0x152f5044240ef872cf7e6742fe202b9e07ed6188e9e734c09b06939704852358 && publicValues[5] == 0x2865441cd3e276643c84e55004ad259dff282c8c47c6e8c151afacdadf6f6db3, "The claim was signed by a wrong public key");
+        require(publicValues[6] == 0x0000000000000000000000000000000087b9b4c689c2024c54d1bf962cb16bce, "Wrong Claim schema used to generate the proof");
+        require(publicValues[7] == 0x0000000000000000000000000000000000000000000000000000000000000002, "Invalid slot index used to generate the proof");
+        require(publicValues[8] == 0x0000000000000000000000000000000000000000000000000000000000000003, "Invalid operator used to generate the proof");
+        require(publicValues[9] == 0x0000000000000000000000000000000000000000000000000000000000000012, "Invalid threshold value used to generate the proof");
 
         require(
-            verifier.verifyProof(a, b, c, input),
+            verifier.verifyProof(a, b, c, publicValues),
             "Proof verification failed"
         );
         isElegibleForAirdrop[msg.sender] = true;
